04a1395f8cd8140e3532e6ab7537ac93eecda9f1
mininuke69 <118979770+mininuke69@users.noreply.github.com>
2023-01-17 18:05:43 +0100

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -1,6 +1,19 @@
 import requests, time
 import tkinter as tk
 
+username = ""
+password = ""
+
+
+def Logintk():
+    global username
+    global password
+    username = usernametk.get()
+    password = passtk.get()
+    
+
+
+
 window = tk.Tk()
 window.title("duinocoin python wallet")
 window.geometry("1600x900")
@@ -8,15 +21,19 @@ window.configure(bg="black")
 
 tk.Label(text="welcome!\n\nUse the form below to log in!",bg="black", foreground="purple", height=5).pack()
 tk.Label(text="Username:", bg="black", fg="purple")
-usernametk = tk.Entry(bg="grey")
+usernametk = tk.Entry()
 usernametk.pack()
+tk.Label(text="password:", bg="black", fg="purple").pack()
+passtk = tk.Entry()
+passtk.pack()
+tk.Button(text="Login", bg="grey", command=Logintk).pack()
 
 
 
 while True:
     time.sleep(0.0)
-    print(usernametk.get())
     window.update()
+    print(username, password)
     
 
 
