119b8c3d92c1a68f612ca5c26c4369dfb00ea3c4
mininuke69 <118979770+mininuke69@users.noreply.github.com>
2023-01-17 17:53:52 +0100

    Create wallet.py

---
+++ wallet.py
@@ -0,0 +1,22 @@
+import requests, time
+import tkinter as tk
+
+window = tk.Tk()
+window.title("duinocoin python wallet")
+window.geometry("1600x900")
+window.configure(bg="black")
+
+tk.Label(text="welcome!\n\nUse the form below to log in!",bg="black", foreground="purple", height=5).pack()
+tk.Label(text="Username:", bg="black", fg="purple")
+usernametk = tk.Entry(bg="grey")
+usernametk.pack()
+
+
+
+while True:
+    time.sleep(0.1)
+    window.update()
+    print(usernametk.get())
+
+window.mainloop()
+
