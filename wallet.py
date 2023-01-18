47511ad3426c88a57a709a704bcc60de0fa45e78
mininuke69 <118979770+mininuke69@users.noreply.github.com>
2023-01-18 14:42:50 +0100

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -5,23 +5,32 @@ username = ""
 password = ""
 userdata = ""
 
+bgcol = "black"
+txtcol = "green"
+errorcol = "red"
+
 def GetUserData():
     global userdata
     userdata = requests.get("https://server.duinocoin.com/users/" + username).json()
 
-def HideLoginForm():
+def LogOn():
     usernameLabel.pack_forget()
     usernameEntry.pack_forget()
     passwordLabel.pack_forget()
     passwordEntry.pack_forget()
     loginbutton.pack_forget()
 
+    balTk = tk.Label(text=userdata["balance"]["balance"], )
+
+
+
+
 def Logintk():
     global username
     global password
 
     response = requests.get("https://server.duinocoin.com/auth/" + usernameEntry.get() + "?password=" + str(passwordEntry.get())).json()
-    loginmessagetk.config(fg="white", text="logging in...")
+    loginmessagetk.config(fg=txtcol, text="logging in...")
     time.sleep(0.5)
     loginmessagetk.config(text="")
     success = response["success"]
@@ -29,12 +38,12 @@ def Logintk():
     if success == True:
         username = usernameEntry.get()
         password = passwordEntry.get()
-        loginmessagetk.config(text=f"Logged in, server: {response['server']}!", fg="white")
-        HideLoginForm()
+        loginmessagetk.config(text=f"Logged in, server: {response['server']}!", fg=txtcol)
+        LogOn()
         
     else:
         error = "login error: " + response["message"]
-        loginmessagetk.config(text=error, fg="red")
+        loginmessagetk.config(text=error, fg=errorcol)
 
 
 
@@ -42,18 +51,18 @@ def Logintk():
 window = tk.Tk()
 window.title("duinocoin python wallet")
 window.geometry("1600x900")
-window.configure(bg="black")
+window.configure(bg=bgcol)
 
-tk.Label(text="welcome!\n\nUse the form below to log in!",bg="black", foreground="purple", height=5).pack()
-usernameLabel = tk.Label(text="Username:", bg="black", fg="purple");usernameLabel.pack()
+tk.Label(text="welcome!\n\nUse the form below to log in!",bg=bgcol, foreground=txtcol, height=5).pack()
+usernameLabel = tk.Label(text="Username:", bg=bgcol, fg=txtcol);usernameLabel.pack()
 usernameEntry = tk.Entry();usernameEntry.pack()
-passwordLabel = tk.Label(text="password:", bg="black", fg="purple");passwordLabel.pack()
+passwordLabel = tk.Label(text="password:", bg=bgcol, fg=txtcol);passwordLabel.pack()
 passwordEntry = tk.Entry();passwordEntry.pack()
-loginmessagetk = tk.Label(text="", bg="black", fg="red");loginmessagetk.pack()
+loginmessagetk = tk.Label(text="", bg=bgcol, fg="red");loginmessagetk.pack()
 loginbutton = tk.Button(text="Login", bg="blue", command=Logintk);loginbutton.pack()
 
 
 
 while True:
     time.sleep(0.0)
-    window.update()    
+    window.update()
