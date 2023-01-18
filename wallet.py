c8b22f12c625009df1ebc3e716e152cabe167a1f
mininuke69 <118979770+mininuke69@users.noreply.github.com>
2023-01-18 15:54:38 +0100

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -22,8 +22,19 @@ def LogOn():
     passwordLabel.pack_forget()
     passwordEntry.pack_forget()
     loginbutton.pack_forget()
+    welcomeLabel.pack_forget()
 
-    balTk = tk.Label(text="Balance: " + str(userdata["result"]["balance"]["balance"]) + "ᕲ", bg=bgcol, fg=txtcol);balTk.pack(side=tk.LEFT, padx=10)
+    balTk = tk.Label(text="Balance: " + str(userdata["result"]["balance"]["balance"]) + "ᕲ", bg=bgcol, fg=txtcol);balTk.pack(anchor="nw", padx=10)
+    txLabel = tk.Label(text="Last transactions:", bg=bgcol, fg=txtcol);txLabel.pack(anchor="nw",padx=10, pady=5)
+    
+    txList = ""
+    for i in range(5):
+        thistx = userdata["result"]["transactions"][i]
+        if thistx["recipient"] == username:
+            txList = txList + "received " + str(thistx["amount"]) + " ᕲ from " + thistx["sender"] + " on " + thistx["datetime"] + " with memo: '" + thistx["memo"] + "' id: " + str(thistx["id"]) + " hash: " + thistx["hash"] + "\n\n"
+        else:
+            txList = txList + "sent " + str(thistx["amount"]) + " ᕲ to " + thistx["sender"] + " on " + thistx["datetime"] + " with memo: '" + thistx["memo"] + "' id: " + str(thistx["id"]) + " hash: " + thistx["hash"] + "\n\n"
+    txListLabel = tk.Label(text=txList, justify=tk.LEFT, bg=bgcol, fg=txtcol);txListLabel.pack(anchor="nw", padx=10)
 
 
 
