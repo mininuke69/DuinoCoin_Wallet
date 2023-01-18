fc2c1feaaca907a7e26f77a2396fd3a9fefa904a
mininuke69 <118979770+mininuke69@users.noreply.github.com>
2023-01-18 13:20:37 +0100

    login works

--- wallet.py
+++ wallet.py
@@ -8,10 +8,25 @@ password = ""
 def Logintk():
     global username
     global password
-    username = usernametk.get()
-    password = passtk.get()
-    
 
+
+    response = requests.get("https://server.duinocoin.com/auth/" + usernametk.get() + "?password=" + str(passtk.get())).json()
+    loginmessagetk.config(fg="white", text="logging in...")
+    time.sleep(0.5)
+    loginmessagetk.config(text="")
+    print(response)
+    success = response["success"]
+    
+    if success == True:
+        username = usernametk.get()
+        password = passtk.get()
+        loginmessagetk.config(text=f"Logged in, server: {response['server']}!", fg="white")
+    else:
+        error = "login error: " + response["message"]
+        loginmessagetk.config(text=error, fg="red")
+
+    
+    
 
 
 window = tk.Tk()
@@ -21,17 +36,16 @@ window.configure(bg="black")
 
 tk.Label(text="welcome!\n\nUse the form below to log in!",bg="black", foreground="purple", height=5).pack()
 tk.Label(text="Username:", bg="black", fg="purple")
-usernametk = tk.Entry()
-usernametk.pack()
+usernametk = tk.Entry();usernametk.pack()
 tk.Label(text="password:", bg="black", fg="purple").pack()
-passtk = tk.Entry()
-passtk.pack()
-tk.Button(text="Login", bg="grey", command=Logintk).pack()
+passtk = tk.Entry();passtk.pack()
+loginmessagetk = tk.Label(text="", bg="black", fg="red");loginmessagetk.pack()
+tk.Button(text="Login", bg="blue", command=Logintk).pack()
 
 
 
 while True:
-    time.sleep(0.0)
+    time.sleep(0.2)
     window.update()
     print(username, password)
     
